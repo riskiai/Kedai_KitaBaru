b6183cc1b45fd24f9c7858d8872307405c39cf26
riskiai <102845635+riskiai@users.noreply.github.com>
2023-02-06 19:02:41 +0700

    Update and rename owner.php to Owner.php

--- app/Owner.php
+++ app/Owner.php
@@ -6,7 +6,7 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class owner extends Authenticatable
+class Owner extends Authenticatable
 {
     use Notifiable;
 
