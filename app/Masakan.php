5046dab8c65e9b8656284a79cff415326e96d8a8
riskiai <102845635+riskiai@users.noreply.github.com>
2023-02-06 19:02:08 +0700

    Update and rename masakan.php to Masakan.php

--- app/Masakan.php
+++ app/Masakan.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class masakan extends Model
+class Masakan extends Model
 {
     public $timestamps = false;
     protected $table = 'tbl_masakan';
